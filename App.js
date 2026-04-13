0799e3423d64483030c5a907e4184cd0f78a52a8
Shellsea Nunez-Aviles <shenunavi126@gmail.com>
2026-04-12 23:01:56 -0400

    bug fixes

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Pressable } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -13,7 +15,17 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerBackVisible: true }}>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={({ navigation }) => ({
+          headerLeft: ({ canGoBack }) =>
+            canGoBack ? (
+              <Pressable onPress={() => navigation.goBack()} style={{ marginRight: 8 }}>
+                <Ionicons name="chevron-back" size={24} color="#ec4899" />
+              </Pressable>
+            ) : null,
+        })}
+      >
         <Stack.Screen
           name="Home"
           component={Home}
